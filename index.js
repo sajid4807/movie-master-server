2bd1c936554ad2315c36bc43f89b56f82b5b75fa
sajid4807 <mdsajidhossen4807@gmail.com>
2025-11-20 22:31:54 +0600

    added all movies api and delete api operation

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const port = process.env.port || 3000;
 
 const uri = "mongodb+srv://movieDBUser:g66So5O6hDUP1hta@cluster0.a47ogqg.mongodb.net/?appName=Cluster0";
 
-
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -29,6 +28,25 @@ async function run() {
 
     const db = client.db('movie_db')
     const moviesCollection = db.collection("movies")
+
+    app.get('/movies',async(req,res) => {
+        const cursor = moviesCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+    app.post('/movies',async(req,res) => {
+        const newMovies = req.body;
+        const result = await moviesCollection.insertOne(newMovies)
+        res.send(result)
+    })
+
+    app.delete('/movies/:id', async(req,res)=> {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await moviesCollection.deleteOne(query)
+        res.send(result)
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
